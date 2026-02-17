175d3731695f75b26aeebff868ae0480882e8c18
ErdemSusam23 <ersusam2003@gmail.com>
2026-02-17 16:17:35 +0300

    Update env.ts

--- frontend/src/config/env.ts
+++ frontend/src/config/env.ts
@@ -1,4 +1,4 @@
-export const API_URL = import.meta.env.VITE_API_URL
+export const API_URL = import.meta.env.VITE_API_URL || "https://nodejs-production-fb070.up.railway.app/api"
 if (!import.meta.env.VITE_API_URL)
   throw new Error("VITE_API_URL missing")
 
